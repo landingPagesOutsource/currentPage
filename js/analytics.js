ea67b278d66ad583fd5d50e3ba0acdbdda55c0cb
Никита <Nikita@Nikitas-Mac-mini.local>
2022-05-18 17:35:50 +0300

    Updates

---
+++ js/analytics.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+
+  // header
+  $(".cta_line_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:line_button",
+      eventLabel: "Start now",
+      eventValue: null,
+    });
+  });
+
+  // main
+  $(".cta_main_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:main_button",
+      eventLabel: "Start now",
+      eventValue: null,
+    });
+  });
+
+  // overview
+  $(".cta_overview_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:overview_button",
+      eventLabel: "Start now",
+      eventValue: null,
+    });
+  });
+
+
+  // cases
+
+  $(".listings-management_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:listings-management_link",
+      eventLabel: "Listing Management",
+      eventValue: null,
+    });
+  });
+
+  $(".domain-overview_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:domain-overview_link",
+      eventLabel: "Domain Overview",
+      eventValue: null,
+    });
+  });
+
+  $(".keyword-overview_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:keyword-overview_link",
+      eventLabel: "Keyword Overview",
+      eventValue: null,
+    });
+  });
+
+  $(".organic-research_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:organic-research_link",
+      eventLabel: "Organic Research",
+      eventValue: null,
+    });
+  });
+
+  $(".keyword-magic-tool_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:keyword-magic-tool_link",
+      eventLabel: "Keyword Magic Tool",
+      eventValue: null,
+    });
+  });
+
+  $(".position-tracking_link").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:position-tracking_link",
+      eventLabel: "Organic Research",
+      eventValue: null,
+    });
+  });
+
+
+
+
+
+  // checks
+  $(".cta_checks_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:all-in-one-local-marketing-solution",
+      eventAction: "click:checks_button",
+      eventLabel: "start now",
+      eventValue: null,
+    });
+  });
+
+});
