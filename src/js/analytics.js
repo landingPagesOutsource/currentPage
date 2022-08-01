ee44c34a56d894ca7c29e40cb1511e4076b2c774
Konstantin Labzov <kosta@Konstantins-Mac-mini.local>
2022-08-01 12:30:42 +0300

    add animation

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   $(".cta_line_button").click(function () {
     dataLayer.push({
       event: "semrush",
-      eventCategory: "lp:grow-small-business-v",
+      eventCategory: "lp:grow-small-business-1",
       eventAction: "click:line_button",
       eventLabel: "try semrush free",
       eventValue: null,
@@ -15,20 +15,81 @@ $(document).ready(function () {
   $(".cta_main_button").click(function () {
     dataLayer.push({
       event: "semrush",
-      eventCategory: "lp:grow-small-business-v",
+      eventCategory: "lp:grow-small-business-1",
       eventAction: "click:main_button",
-      eventLabel: "try semrush free",
+      eventLabel: "try it free",
       eventValue: null,
     });
   });
 
-  // cta
-  $(".cta_cta_button").click(function () {
+  // tools
+  $(".cta_tools-1_button").click(function () {
     dataLayer.push({
       event: "semrush",
-      eventCategory: "lp:grow-small-business-v",
-      eventAction: "click:cta_button",
-      eventLabel: "try semrush free",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-1_button",
+      eventLabel: "Learn more about seo",
+      eventValue: null,
+    });
+  });
+
+  $(".cta_tools-2_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-2_button",
+      eventLabel: "Reach your local audience",
+      eventValue: null,
+    });
+  });
+
+  $(".cta_tools-3_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-3_button",
+      eventLabel: "X-ray your competition",
+      eventValue: null,
+    });
+  });
+
+  $(".cta_tools-4_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-4_button",
+      eventLabel: "Amp up your social media",
+      eventValue: null,
+    });
+  });
+
+  $(".cta_tools-5_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-5_button",
+      eventLabel: "Win more Amazon sales",
+      eventValue: null,
+    });
+  });
+
+  $(".cta_tools-6_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:tools-6_button",
+      eventLabel: "Spend your dollars wisely",
+      eventValue: null,
+    });
+  });
+
+  // help
+  $(".cta_help_button").click(function () {
+    dataLayer.push({
+      event: "semrush",
+      eventCategory: "lp:grow-small-business-1",
+      eventAction: "click:help_button",
+      eventLabel: "Learn more",
       eventValue: null,
     });
   });
@@ -37,9 +98,9 @@ $(document).ready(function () {
   $(".cta_footer_button").click(function () {
     dataLayer.push({
       event: "semrush",
-      eventCategory: "lp:grow-small-business-v",
+      eventCategory: "lp:grow-small-business-1",
       eventAction: "click:footer_button",
-      eventLabel: "try semrush free",
+      eventLabel: "try it free",
       eventValue: null,
     });
   });
